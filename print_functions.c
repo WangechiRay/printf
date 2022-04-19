fefe1efa19518f4b2cde5f14857b12486b33e8d8
joynatalie <99263754+joynatalie@users.noreply.github.com>
2022-04-20 01:16:43 +0300

    printf

--- print_functions.c
+++ print_functions.c
@@ -1,43 +1,104 @@
 #include "main.h"
-
 /**
- * print_char - prints character
- * @list: list of arguments
- * Return:Will return the number of characters printed
+ * print_character - prints character
+ * @arg: va_list parameter
+ * Description: print character
+ * Return: 1
  */
-
-int print_char(va_list list)
+int print_character(va_list arg)
 {
-_write_char(va_arg(list, int));
-return (1);
+	int i;
+
+	i = va_arg(arg, int);
+	_putchar(i);
+
+	return (1);
 }
-
 /**
- * print_string - prints string
- * @list: list of arguments
- * Return: Will return the number of characters printed
+ * print_sign - print sign
+ * @arg: va_list parameter
+ * @base: base 10, 8, 16, 2 etc..
+ * Description: print numbers and signed
+ * Return: num of characters
  */
-
-int print_string(va_list list)
+int print_sign(va_list arg, int base)
 {
-int i;
-char *str;
-str = va_arg(list, char *);
-if (str == NULL)
-str = "(null)";
-for (i = 0; str[i] != '\0', i++)
-_write_char(str[i]);
-return (i);
+	int i = 0, cont = 0;
+	char *s;
+
+	i = va_arg(arg, int);
+	if (i < 0)
+	{
+		i = -(i);
+		_putchar('-');
+		cont += 1;
+	}
+	s = convert_to("0123456789ABCDEF", i, base);
+	_puts(s);
+	cont += _strlen(s);
+	return (cont);
 }
-
 /**
- *print_percent - prints a percent symbol
- *@list: list of arguments
- *Return: Will return the number of characters printed
+ * print_unsign - print_unsign
+ * @arg: va_list parameter
+ * @base: base 10, 8, 16 etc..
+ * Description: print numbers without signed
+ * Return: num of characters
  */
-
-int print_percent(__attribute__((unused))va_list list)
+int print_unsign(va_list arg, int base)
 {
-_write_char('%');
-return (1);
+	int cont = 0;
+	unsigned int i;
+	char *s;
+
+	i = va_arg(arg, unsigned int);
+	s = convert_to("0123456789ABCDEF", i, base);
+	_puts(s);
+	cont = _strlen(s);
+
+	return (cont);
+
+}
+/**
+ * print_string - print string
+ * @arg: va_list parameter
+ * Description: print string
+ * Return: num of characters
+ */
+int print_string(va_list arg)
+{
+	char *s;
+	int cont = 0;
+
+	s = va_arg(arg, char *);
+	if (!s)
+	{
+		s = "(null)";
+		_puts(s);
+
+		return (_strlen(s));
+	}
+	_puts(s);
+	cont = _strlen(s);
+	return (cont);
+}
+/**
+ * print_base16_upper_lower - print_base16_upper_lower
+ * @arg: va_list parameter
+ *@representation: pointer parameter
+ * Description: This function takes 0123456789ABCDEF or 0123456789abcdef
+ * in representation parameter for print hexadecimal format
+ * Return: num of characters
+ */
+int print_base16_upper_lower(va_list arg, char *representation)
+{
+	unsigned int i = 0, cont = 0;
+	char *s;
+
+	i = va_arg(arg, unsigned int);
+	s = convert_to(representation, i, 16);
+	_puts(s);
+	cont = _strlen(s);
+	return (cont);
+
 }
