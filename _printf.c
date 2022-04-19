a07f102b3b71b5b98968f563cba2c77bd48976c0
wangechi <rahabsheshi22@gmail.com>
2022-04-19 17:54:06 +0000

    printf function

--- _printf.c
+++ _printf.c
@@ -1,49 +1,89 @@
 #include "main.h"
-
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-        _printf("my printf function");
-        return (0);
-}
-
-
+#include <stdarg.h>
 #include <stdio.h>
-#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
 
 /**
- * _prinf - prints a function that  produces output to a format
- * @format: character string 
+ * _printf-function that produces output according to a format.
+ * @format:character string
  *
  * Return: number of characters printed
  */
 
-int _printf(const char *format, ...)
+int (*get_op(const char c))(va_list)
 {
-	int x;
-	char c ,*str;
-	va_list list;
+	int i = 0;
 
-	va_start(list, format);
-
-	while (*format)
-		switch (*format++){
-			case 's':
-				s = va_arg(list, char *);
-				printf("%s\n", s);
-				break;
-				
-			case 'c':
-				c = (char) va_arg(list, int);
-				printf("char %c\n", c)
-					break;
+	flags_p fp[] = {
+		{"c", print_char},
+		{"s", print_str},
+		{"i", print_nbr},
+		{"d", print_nbr},
+		{"b", print_binary},
+		{"o", print_octal},
+		{"x", print_hexa_lower},
+		{"X", print_hexa_upper},
+		{"u", print_unsigned},
+		{"S", print_str_unprintable},
+		{"r", print_str_reverse},
+		{"p", print_ptr},
+		{"R", print_rot13},
+		{"%", print_percent}
+	};
+	while (i < 14)
+	{
+		if (c == fp[i].c[0])
+		{
+			return (fp[i].f);
 		}
-	va_end(list);
+		i++;
+	}
+	return (NULL);
 }
 
+/**
+ * _printf - Reproduce behavior of printf function
+ * @format: format string
+ * Return: value of printed chars
+ */
 
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int sum = 0, i = 0;
+	int (*func)();
+
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	va_start(ap, format);
+
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] != '\0')
+				func = get_op(format[i + 1]);
+			if (func == NULL)
+			{
+				_putchar(format[i]);
+				sum++;
+				i++;
+			}
+			else
+			{
+				sum += func(ap);
+				i += 2;
+				continue;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			sum++;
+			i++;
+		}
+	}
+	va_end(ap);
+	return (sum);
+}
--- main.h
+++ main.h
@@ -1,9 +1,46 @@
 #define MAIN_H
 #ifndef MAIN_H
 
-#include <stdarh.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int _printf(const char *format, ...);
+int print_char(va_list ap);
+int print_str(va_list ap);
+int print_nbr(va_list ap);
+int print_binary(va_list ap);
+int print_octal(va_list ap);
+int print_hexa_lower(va_list ap);
+int print_hexa_upper(va_list ap);
+int print_unsigned(va_list ap);
+int print_str_unprintable(va_list ap);
+int print_str_reverse(va_list ap);
+int print_ptr(va_list ap);
+int print_rot13(va_list ap);
+int print_percent(va_list ap __attribute__((unused)));
+int print_number(int n);
+int print_unsigned_number(unsigned int n);
+int _putchar(char c);
+int _puts(char *str, int ascii);
+int _nbr_len(int prmNumber);
+int _strlen_recursion(char *s);
+int convert_alpha_numeric(int nb, int upper);
+char *convert_rot13(char *str);
+char *convert_base(unsigned long nb, unsigned int base, int upper);
+char *_strdup(char *str);
+char *convert_base_pointer(unsigned long p);
 
+/**
+ * struct specifier - struct specifier
+ * @valid: the valid character.
+ * @f: the functions associated.
+ *
+ */
+typedef struct specifier
+{
+	char *valid;
+	int (*f)(va_list);
+} flags_p;
 
 #endif
