1805d5de8720ed32665ec072db490e90780c0ab2
wangechi <rahabsheshi22@gmail.com>
2022-04-19 09:06:03 +0000

    prinf

--- _printf.c
+++ _printf.c
@@ -1,18 +1,49 @@
+#include "main.h"
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        _printf("my printf function");
+        return (0);
+}
+
+
 #include <stdio.h>
 #include "main.h"
 
 /**
  * _prinf - prints a function that  produces output to a format
- * @format: charactr string 
+ * @format: character string 
  *
  * Return: number of characters printed
  */
 
-int main(void)
-{
-	char x;
-
 int _printf(const char *format, ...)
 {
 	int x;
+	char c ,*str;
+	va_list list;
+
+	va_start(list, format);
+
+	while (*format)
+		switch (*format++){
+			case 's':
+				s = va_arg(list, char *);
+				printf("%s\n", s);
+				break;
+				
+			case 'c':
+				c = (char) va_arg(list, int);
+				printf("char %c\n", c)
+					break;
+		}
+	va_end(list);
+}
+
 
--- main.h
+++ main.h
@@ -1,6 +1,8 @@
 #define MAIN_H
 #ifndef MAIN_H
 
+#include <stdarh.h>
+
 int _printf(const char *format, ...);
 
 
